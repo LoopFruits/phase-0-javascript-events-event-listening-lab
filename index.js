3c5df0a62e4c06e8c0e9649c13c15765f96f4570
LoopFruits <Kensworldc@gmail.com>
2022-02-07 11:11:47 -0500

    complete assign with notes

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const input = document.getElementById('input');
 input.addEventListener('click',() => alert('Hi I was Clicked!'))
 } 
   
-
+// line 3 can also be written as so
+// input.addEventListener('click', function(){
+// alert('I was clicked');   
+//});
